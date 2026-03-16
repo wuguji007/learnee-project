2d8290e276a3f1c8ef2b64b432464d817b30756a
kkchen <alie40521@gmail.com>
2026-03-16 19:25:22 +0800

    test: mock swiper library to fix jest ESM module error

--- client/src/tests/Home-component.test.js
+++ client/src/tests/Home-component.test.js
@@ -3,6 +3,24 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import HomeComponent from '../components/Home-component';
 
+
+// 1. 模擬 swiper/react 套件的元件
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }) => <div data-testid="swiper-mock">{children}</div>,
+  SwiperSlide: ({ children }) => <div data-testid="swiper-slide-mock">{children}</div>,
+}));
+
+// 2. 模擬 swiper/modules 套件的功能
+jest.mock('swiper/modules', () => ({
+  Navigation: () => null,
+  Pagination: () => null,
+  A11y: () => null,
+  Autoplay: () => null, // 如果你的輪播圖有用到 autoplay 也可以加上
+}));
+
+// CRA 預設會自動忽略 CSS import，所以 swiper/css 等引入不會造成報錯
+
+
 describe('HomeComponent', () => {
   it('renders without crashing', () => {
     // 因為你的元件中很可能有使用到 <Link> 或 useNavigate，
